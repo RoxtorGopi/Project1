457160538b439471f3927b166011e7bc79579264
RoxtorGopi <roxtorgopi815@gmail.com>
2024-01-09 16:33:54 +0530

    Simple Java Code

---
+++ src/charani_Package/Charani_Class2.java
@@ -0,0 +1,17 @@
+package charani_Package;
+
+public class Charani_Class2 {
+	public static void main(String[] args) {
+		
+		int Age=23;
+		if(Age>18)
+		{
+			System.out.println("Person can be eligible to vote");
+		}
+		else
+		{
+			System.out.println("Person can't be eligible to vote");
+		}
+	}
+
+}
